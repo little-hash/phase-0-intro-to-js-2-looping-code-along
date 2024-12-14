09a62f95106197bb95865b25b6d1ded1207a3614
Ezra <ezraraddai166@gmail.com>
2024-12-14 16:37:12 +0300

    firstcommit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards (namesarray, event){
+    let thankyouCards = []
+    for(let i = 0; i < namesarray.length; i++){
+        thankyouCards.push(`thank you, ${namesarray[1]}, for the wonderful ${event} gift!`)
+
+    }
+    return thankyouCards
+}
+
+function countDown( startingNumber){
+    while(startingNumber > 0){
+        console.log(startingNumber)
+        startingNumber = 1
+    }
+    console.log(startingNumber)
+}
